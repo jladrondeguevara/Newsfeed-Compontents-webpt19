3baf38a0f87b6d0675795bd923762ae1878520ca
Jeovanni <jeovannildg@gmail.com>
2020-07-14 21:36:51 -0600

    created elements in article file

--- components/Article.js
+++ components/Article.js
@@ -111,3 +111,15 @@ const data = [
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 */
+
+const articleMaker = () =>{
+  const article = document.createElement('div');
+  const title = document.createElement('h2');
+  const date = document.createElement('p');
+  const par1 = document.createElement('p');
+  const par2 = document.createElement('p');
+  const par3 = document.createElement('p');
+
+
+  return article;
+}
